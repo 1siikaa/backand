fd45c1ce380dfc528b20f53c8e82044d05ac035e
Vanshika Dubey / <srishtidwivedi672@gmail.com>
2023-04-16 18:31:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-
-
-
-
-
-
-    
-
-
-    
 const express = require("express");
 const route = require('./src/route/route.js')
 const mongoose = require('mongoose');
 const app = express();
-const cors = require('cors')
-app.use(cors())
+app.use((req, res, next)=>{
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
+
+
+
 app.use(express.json())
 
 
